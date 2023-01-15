2e9ea3f2737659b6cd3519c7bad59735a12e0e16
Logan T Hall <73491910+loganthall@users.noreply.github.com>
2023-01-15 13:27:20 -0500

    commit package module and init file

---
+++ escape_pkg/all_options.py
@@ -0,0 +1,14 @@
+movement = ["\nYou begin running as fast as possible down a dimly lit hallway to your left.\n",
+            "\nYou climb over the stack of boxes in front of you, seemingly stacked there to prevent you from travelling this direction.\n",
+            "\nYou run toward the direction of the next light, hoping the warm glow leads to freedom. On your way, you notice you have less traction as the floor is covered in a slippery substance.\n",
+            "\nIn a rush of adrenaline top make it out alive, you kick through a broken piece of wall in front of you, opening a passage to a different room\n",
+            "\nWhat appears to be a mirror sits on the floor to your left. In the opposite corner of the room, you spot a g0lf club. You shatter the mirror with it, opening a small passageway that leads to a different part of the facility.\n",
+            "\nYou decide to turn around and face the one that brought you here. Not only are they larger than you, their eyes somehow glow black with darkess.\n"]
+
+movement_selectors = ["Head through the hallway", "Climb the boxes", "Run toward the light",
+                      "Kick the cracked wall", "Break the mirror", "Fight your assailant"]
+
+random_actions = ["none", "You find a random piece of pipe laying near you. Grabbing it, you swing with all your force and immediately make contact with your assailant's face, stumbling them and opening your chance to move.", "none",
+                  "In a brief moment of clarity, you notice the faded color of oRange that covers the walls.", "none", "In what seemed like a half of a second, hands materialized around your neck, squeezing harder than anything you've felt before. It doesn't take long before the cold grip of death takes you."]
+
+secret_words = {"oRange", "g0lf"}
---
+++ escape_pkg/randomizer.py
@@ -0,0 +1,84 @@
+import random
+from escape_pkg import all_options
+
+
+def randomize_movement():  # function that randomizes the movement options
+    # holds randomized movement selectors so that the original list is not mutated
+    movement_options = []
+    # holds randomized movement description that are aligned with the selectors so that the original list is not mutated
+    movement_descriptions = []
+
+    iter = 0  # sets iteration variable
+    while iter < 3:  # iterates process of adding options to the non-global variables to allow 3 choices
+        # generates random number to select index of an option/description set
+        indexes = random.randint(0, len(all_options.movement_selectors) - 1)
+        # checks for repeated items in the variable, skips without increasing iteration varibale if exists
+        if all_options.movement_selectors[indexes] in movement_options:
+            continue
+        # appends randomly chosen item to the variable for user choice
+        movement_options.append(all_options.movement_selectors[indexes])
+        # checks for repeated items in the variable, skips without increasing iteration varibale if exists
+        if all_options.movement[indexes] in movement_descriptions:
+            continue
+        # appends randomly chosen descriptions that are aligned with the users choice
+        movement_descriptions.append(all_options.movement[indexes])
+
+        iter += 1  # adds iteration to the loop
+
+    iter = 0  # resets iteration variable
+    while iter < 3:
+        print(str(iter + 1), ". " + movement_options[iter])
+        iter += 1
+
+    # requests users choice of movement, and returns description accordingly
+    user_choice = input("Choose your next move: ")
+    if user_choice == "1":
+        return movement_descriptions[0]
+    elif user_choice == "2":
+        return movement_descriptions[1]
+    elif user_choice == "3":
+        return movement_descriptions[2]
+    elif user_choice in all_options.secret_words:  # if user enters a secret word, this happens
+        print("\nYou spoke the secret word and the world around you seems to materialize into something more familiar. You slowly fade in and out of consciousness, and when you finally wake up you are back home in your bed. It was all a bad dream.")
+        return False
+    else:  # if nothing valid is entered, this is displayed
+        print("\nPlease enter a valid choice.\n")
+        return None
+
+
+def randomize_actions():  # function to randomly choose, and randomly disperse, favorful or debilitating actions
+    # chooses random number between 0 and 5 for the index of the action
+    indexes = random.randint(0, len(all_options.random_actions) - 1)
+    if indexes % 2 == 1:  # checks if index is even or odd. even indexes are null scenarios, so only odds have data
+        if indexes == 5:
+            consequence = "Your life has come to an untimely end.\n"
+        elif indexes % 2 == 1:
+            consequence = "You escape to the next area safely.\n"
+        else:
+            consequence = None
+        return all_options.random_actions[indexes], consequence
+    else:  # returns None for both variables if the index is even
+        return None, None
+
+
+def life_or_death():  # Function to randomize life or death when facing your foe
+    coin = random.randint(0, 1)
+    if coin == 0:  # if the coin toss lands on 0, the result is heads, which is living
+        consequence = "heads"
+        return consequence
+    elif coin == 1:  # if the coin toss lands on 1, the result is tails, which is death
+        consequence = "tails"
+        return consequence
+
+
+def play_again():  # function to ask user if they would like to play again or not
+    play_again_choice = input("\nWould you like to play again? (y/n)")
+    play_again_choice = play_again_choice.lower()  # converts input to lowercase for easier comparison
+    if play_again_choice == "y" or play_again_choice == "yes":
+        return True
+    elif play_again_choice == "n" or play_again_choice == "no":
+        print("\nThank you for playing.")
+        return exit()
+    else:
+        print("\nPlease enter a valid option.")
+        play_again()  # calls the function to run again if no valid input it entered
