83e72ec7f998bdccbb08454ee7bb2bd20b8ed554
Logan T Hall <73491910+loganthall@users.noreply.github.com>
2023-01-15 21:29:12 -0500

    upload new versions
    upload new versions of all_options.py and randomizer.py

---
+++ escape_pkg/all_options.py
@@ -0,0 +1,22 @@
+movement = ("\nYou begin running as fast as possible down a dimly \nlit hallway to your left.\n",
+            "\nYou climb over the stack of boxes in front of \nyou, seemingly stacked there to prevent you from \ntravelling this direction.\n",
+            "\nYou run toward the direction of the next light, \nhoping the warm glow leads to freedom. On your way, \nyou notice you have less traction as the floor is \ncovered in a slippery substance.\n",
+            "\nIn a rush of adrenaline top make it out alive, \nyou kick through a broken piece of wall in front of you, \nopening a passage to a different room\n",
+            "\nWhat appears to be a mirror sits on the floor to \nyour left. In the opposite corner of the room, you spot a \ng0lf club. You shatter the mirror with it, \nopening a small passageway that leads to a different part \nof the facility.\n",
+            "\nYou decide to turn around and face the one that \nbrought you here. Not only are they larger than you, their \neyes somehow glow black with darkess.\n")
+
+movement_selectors = ("Head through the hallway",
+                      "Climb the boxes", "Run toward the light",
+                      "Kick the cracked wall",
+                      "Break the mirror",
+                      "Fight your assailant")
+
+random_actions = ("none",
+                  "You find a random piece of pipe laying near \nyou. Grabbing it, you swing with all your force and \nimmediately make contact with your assailant's face, \nstumbling them and opening your chance to move.\n",
+                  "none",
+                  "In a brief moment of clarity, you notice the \nfaded color of oRange that covers the walls.\n",
+                  "none",
+                  "In what seemed like a half of a second, hands \nmaterialized around your neck, squeezing harder than \nanything you've felt before. It doesn't take long before \nthe cold grip of death takes you.\n")
+
+secret_words = {"oRange",
+                "g0lf"}
